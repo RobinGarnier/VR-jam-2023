2c9cca0814b27a6a2bd2ebfaefaf63c788a82ff9
RobinGarnier <Robin.GARNIER@ecam-strasbourg.eu>
2023-06-27 17:55:44 +0200

    modification Day2 last on the day

--- Assets/Scripts/Deplace.cs
+++ Assets/Scripts/Deplace.cs
@@ -8,7 +8,6 @@ public class Deplace : MonoBehaviour
 
     [Header("Car RoadBook")]
     [Tooltip("Pas touche")]public Vector3 trajetA = Vector3.zero;
-    Vector3 trajetB = Vector3.zero;
     [Tooltip("Pas touche")] public bool tripFinished;
 
     [Header("Nav Proporties")]
@@ -18,10 +17,21 @@ public class Deplace : MonoBehaviour
     [Tooltip("Pas touche : return of the current destination")] public GameObject destination;
     [Tooltip("Start the navigation with it")]public bool launched;
 
-
+    [Header("Checking and bug clearing ")]
+    [Tooltip("Delay between each StuckCheck")][Range(0.5f,5f)] public float timerStuck;
+    Vector3 positionPasse;
+    float resteTimerStuck;
+    public GameObject sun;
+    public float rotateDelay;
+    bool rotate;
+    
+    
+    
+    
     void Awake()
     {
         agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
 
         destinationsPossible = GameObject.FindGameObjectsWithTag("drivable roads");
 
@@ -39,39 +49,77 @@ public class Deplace : MonoBehaviour
             resteDestinationPossible.RemoveAt(indexRandom);
         }
 
+
         destination.transform.position = destinationGlobal[0].transform.position;
         SetPosition(destination);
+
+
+        resteTimerStuck = timerStuck;
     }
 
 
     void Update()
     {
         agent.SetDestination(trajetA);
-        
+
+       
         if (launched)
         {
             { trajetA = destination.transform.position; }
         }
 
+
         //if ((transform.position.x - 0.1<= destination.transform.position.x || destination.transform.position.x <= transform.position.x+0.1) && (destination.transform.position.z-0.1<= transform.position.z || transform.position.z <= destination.transform.position.z+ 0.1))
-        if (transform.position.x== destination.transform.position.x && destination.transform.position.x == transform.position.x)
+        if (transform.position.x== destination.transform.position.x && destination.transform.position.z == transform.position.z)
         {
-            try 
+            NvlleDirection();
+        }
+
+        if (launched)
+        {
+            resteTimerStuck-= Time.deltaTime;
+            if (resteTimerStuck < 0)
             {
-                destinationGlobal.RemoveAt(0);
-                destination.transform.position = destinationGlobal[0].transform.position;
-                SetPosition(destination);
+                StuckChecking(transform.position);
+                positionPasse = transform.position;
+                resteTimerStuck = timerStuck;
+                rotate = false;
             }
-            catch
+            if (rotate)
             {
-                tripFinished = true;
+                sun.transform.Rotate((int)((140 / (nbEtape * 5)) * Time.deltaTime), 0, 0);
             }
         }
+
+        
     }
 
     void SetPosition(GameObject destination)
     {
         destination.transform.position = new Vector3(destination.transform.position.x, 25 , destination.transform.position.z);
     }
+    public void StuckChecking(Vector3 position)
+    {
+        if (position == positionPasse)
+        {
+            NvlleDirection();
+        }
+    }
 
+    public void NvlleDirection()
+    {
+        try
+        {
+            destinationGlobal.RemoveAt(0);
+            destination.transform.position = destinationGlobal[0].transform.position;
+            SetPosition(destination);
+            rotate = true;
+        }
+        catch
+        {
+            tripFinished = true;
+        }
+    }
+
+    
 }
